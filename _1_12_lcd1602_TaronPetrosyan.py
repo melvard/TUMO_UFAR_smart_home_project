732a5de53cacfeb195e0daa55bb0d0932729d2ee
Vardan <69721477+melvard@users.noreply.github.com>
2021-12-23 21:53:10 +0400

    Updated function

--- _1_12_lcd1602_TaronPetrosyan.py
+++ _1_12_lcd1602_TaronPetrosyan.py
@@ -92,54 +92,5 @@ import time
 
 lcd = LCD() # params available for rPi revision, I2C Address, and backlight on/off
             # lcd = LCD(2, 0x3F, True)
-
-lcd.message("Hello World!", 1) # display 'Hello World!' on line 1 of LCD
-
-time.sleep(5) # wait 5 seconds
-
-lcd.clear() # clear LCD displaylcd class
-lcd = character_lcd.Character_LCD_Mono(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6,
-                                      lcd_d7, lcd_columns, lcd_rows)
-
-# looking for an active Ethernet or WiFi device
-def find_interface():
-    find_device = "ip addr show"
-    interface_parse = run_cmd(find_device)
-    for line in interface_parse.splitlines():
-        if "state UP" in line:
-            dev_name = line.split(':')[1]
-    return dev_name
-
-# find an active IP on the first LIVE network device
-def parse_ip():
-    find_ip = "ip addr show %s" % interface
-    find_ip = "ip addr show %s" % interface
-    ip_parse = run_cmd(find_ip)
-    for line in ip_parse.splitlines():
-        if "inet " in line:
-            ip = line.split(' ')[5]
-            ip = ip.split('/')[0]
-    return ip
-
-# run unix shell command, return as ASCII
-def run_cmd(cmd):
-    p = Popen(cmd, shell=True, stdout=PIPE)
-    output = p.communicate()[0]
-    return output.decode('ascii')
-
-# wipe LCD screen before we start
-lcd.clear()
-
-# before we start the main loop - detect active network device and ip address
-sleep(2)
-interface = find_interface()
-ip_address = parse_ip()
-
-while True:
-    # date and time
-    lcd_line_1 = datetime.now().strftime('%b %d  %H:%M:%S\n')
-    # current ip address
-    lcd_line_2 = "IP " + ip_address
-    # combine both lines into one update to the display
-    lcd.message = lcd_line_1 + lcd_line_2
-
+def send_message():
+    lcd.message()
